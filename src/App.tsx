01e7c303c07a64cddcf62eaf4d7db38a0488a247
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-09-06 17:08:45 -0400

    Initial boilerplate website

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to My Website</h1>
+      <div className="card">
+        <p>
+          Come back soon to learn more about me, Josh Lawrence
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Soon I will share a bit about me and what it is I do!
+      </p>
+    </>
+  )
+}
+
+export default App

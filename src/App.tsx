487c83007c047163031c4bd9be3e383dfb56302b
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-09-09 16:21:43 -0400

    Modified content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
 
+// React Component Test
+function TestButton() {
+  return (
+    <button onClick={() => alert('This is a button!')}>Push Me</button>
+  ); 
+}
+
 function App() {
   return (
     <>
+      <div className="image-cropper">
+        <img src="/Josh_Lawrence_Headshot.JPG" alt="Picture" className="headshot"/>
+        </div>
       <h1>Welcome to My Website</h1>
       <h3>About Me</h3>
-      <p>Don't worry, I'm not a frontend dev!</p>
+      <p>Hi, I'm Josh! Don't worry, I'm not a frontend dev. </p>
       <h3>Accomplishments</h3>
       <ul>
         <li>Presidents Award</li>
@@ -24,6 +34,7 @@ function App() {
       <p className="read-the-docs">
         Soon I will share a bit about me and what it is I do!
       </p>
+      <TestButton />
     </>
   )
 }
